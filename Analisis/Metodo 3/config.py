ab54d5fc4e71b59a3a1a69c2f154e5ed1e46ea22
aduralbizu <adur.albizu@gmail.com>
2026-09-04 19:51:53 +0200

    REF - Umbral puertos únicos definitivo

--- Analisis/Metodo 3/config.py
+++ Analisis/Metodo 3/config.py
@@ -45,9 +45,7 @@ PATRON_UDP = "_udp_"
 ARCHIVO_RESULTADO = "resultado.txt"
 ARCHIVO_RESULTADO_JSON = "resultado_metodo3.json"
 
-LIMITE_PUERTOS= 10 #Limite por debajo del cual supongo más dudoso un escaneo de puertos
-
-THRESHOLD_PUERTOS_UNICOS = 20 # Limite por encima del cual considero que una conversación podría ser un escaneo
+THRESHOLD_PUERTOS_UNICOS = 10 # Limite por encima del cual considero que una conversación podría ser un escaneo
 THRESHOLD_ENTROPIA = 0.98 # Limite de h por encima del cual considero que una conversación podría ser un escaneo
 THRESHOLD_PROPORCION_SONDEOS_TCP = 0.9 # La proporción de flujos TCP compatibles con un sondeo debe superar este valor
 THRESHOLD_PROPORCION_SONDEOS_UDP = 0.9 # Proporción mínima de flujos UDP sin respuesta
